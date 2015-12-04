2229790c9a8f78004fb79a1b8af70ccc44b328a6
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2015-12-04 16:53:56 -0500

    Added filename constants

---
+++ const/filename_consts.py
@@ -0,0 +1,8 @@
+# Contains constants to filenames.
+
+# The tcpdump trace from the phone.
+PCAP_FILENAME = 'output.pcap'
+# Total bytes filename.
+TOTAL_BYTES_FILENAME = 'bytes_received.txt'
+# The utilization filename.
+UTILIZATION_FILENAME = 'bandwidth.txt'
