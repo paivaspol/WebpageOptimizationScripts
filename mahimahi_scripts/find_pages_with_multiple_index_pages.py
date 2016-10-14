32c6a5c38ee597b4738f460cee37316747003a00
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2016-10-14 10:46:50 -0400

    Fixed bug in find_pages_with_multiple_index_pages.py

--- mahimahi_scripts/find_pages_with_multiple_index_pages.py
+++ mahimahi_scripts/find_pages_with_multiple_index_pages.py
@@ -7,6 +7,8 @@ def main(root_dir, page_to_timestamp_filename):
     page_to_timestamp_mapping = get_page_to_timestamp_mapping(page_to_timestamp_filename)
     for page, timestamp in page_to_timestamp_mapping:
         page_directory = os.path.join(root_dir, timestamp, page)
+        if not os.path.exists(page_directory):
+            continue
         index_files = find_index_files(page_directory)
         if len(index_files) > 1:
             print '{0} {1}'.format(page, len(index_files))
@@ -15,13 +17,14 @@ def find_index_files(page_directory):
     recorded_files = os.listdir(page_directory)
     result = []
     for recorded_file in recorded_files:
-        top_cmd = './generate_pages_with_scheduling_logic/protototext {0} {1}'.format(recorded_file, 'temp.out')
+        full_path_to_recorded_file = os.path.join(page_directory, recorded_file)
+        top_cmd = './generate_pages_with_scheduling_logic/protototext {0} {1}'.format(full_path_to_recorded_file, 'temp.out')
         proc_top = subprocess.Popen([top_cmd], stdout=subprocess.PIPE, shell=True)
         (out_top, err_top) = proc_top.communicate()
         out_top = out_top.strip("\n")
         if ( "type=htmlindex" in out_top ): # this is the top-level HTML
             top_level_html = out_top.split("na--me=")[1]
-            result.append(filename)
+            result.append(recorded_file)
 
         rm_cmd = 'rm temp.out'
         subprocess.call(rm_cmd, shell=True)
