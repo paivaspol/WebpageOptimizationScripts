0de0dcfe637897eb201474a606509f3cf697a959
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2017-05-18 13:57:58 -0400

    Updated visual metrics scripts

---
+++ aft_debug/aggregate_visual_metrics.py
@@ -0,0 +1,38 @@
+from argparse import ArgumentParser
+
+import os
+
+def main(root_dir):
+    for p in os.listdir(root_dir):
+        visual_metrics_filename = os.path.join(root_dir, p, 'visual_metrics')
+        with open(visual_metrics_filename, 'rb') as input_file:
+            v = -1
+            dont_print = False
+            for l in input_file:
+                if l.startswith('Speed') and args.mode == 'SpeedIndex':
+                    v = l.strip().split(':')[1].strip()
+                    break
+                elif l.startswith('Visual') and (args.mode == 'AFT' or args.mode == 'AFT_no_offset'):
+                    tokens = l.strip().split(', ')
+                    for t in tokens:
+                        splitted_t = t.split('=')
+                        try:
+                            if splitted_t[1] == '100%':
+                                if args.mode == 'AFT':
+                                    v = int(splitted_t[0]) - 2000
+                                else:
+                                    v = int(splitted_t[0])
+                                break
+                        except:
+                            dont_print = True
+                            break
+            if not dont_print:
+                print '{0} {1}'.format(p, v)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('root_dir')
+    parser.add_argument('mode', choices=[ 'SpeedIndex', 'AFT', 'AFT_no_offset' ])
+    args = parser.parse_args()
+    main(args.root_dir)
--- aft_debug/compute_visual_metrics.py
+++ aft_debug/compute_visual_metrics.py
@@ -1,22 +1,38 @@
 from argparse import ArgumentParser
+from multiprocessing import Pool
 
+import itertools
 import os
 import subprocess
 
-def main(root_dir):
-    for p in os.listdir(root_dir):
-        plt = get_plt(os.path.join(root_dir, p, 'start_end_time_' + p))
-        input_filename = os.path.join(root_dir, p, 'screen_record.mp4')
-        cmd = 'python /Users/vaspol/Documents/research/MobileWebPageOptimization/binaries/visualmetrics/visualmetrics.py --start 2000 --end {0} -i {1} --full'.format((plt + 1500), input_filename)
-        output = subprocess.check_output(cmd.split())
-        print 'here: ' + output
-        speedindex, aft = parse_visual_metrics(output)
-        print '{0} {1} {2}'.format(p, speedindex, aft)
-        break
+def main(root_dir, output_dir):
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    worker_pool = Pool(4)
+    worker_pool.map(process_page_wrapper, itertools.izip( \
+                                            itertools.repeat(root_dir), \
+                                            os.listdir(root_dir), \
+                                            itertools.repeat(output_dir)))
+
+def process_page_wrapper(args):
+    process_page(*args)
+
+def process_page(root_dir, p, output_dir):
+    plt = get_plt(os.path.join(root_dir, p, 'start_end_time_' + p))
+    input_filename = os.path.join(root_dir, p, 'screen_record.mp4')
+    page_output_dir = os.path.join(output_dir, p)
+    if not os.path.exists(page_output_dir):
+        os.mkdir(page_output_dir)
+    cmd = 'python /Users/vaspol/Documents/research/MobileWebPageOptimization/binaries/visualmetrics/visualmetrics.py --start 2000 --end {0} -i {1} --full --dir {2}'.format((plt + 1500 + 2000), input_filename, page_output_dir)
+    output = subprocess.check_output(cmd.split())
+    with open(os.path.join(page_output_dir, 'visual_metrics'), 'wb') as output_file:
+        output_file.write(output)
+
 
 def parse_visual_metrics(visual_metric_str):
     speedindex = -1
     aft = -1
+    splitted_visual_metric_str = visual_metric_str.strip().split('\n')
     for l in visual_metric_str:
         if l.startswith('Speed '):
             speedindex = l.split(':')[1].strip()
@@ -32,5 +48,6 @@ def get_plt(filename):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('root_dir')
+    parser.add_argument('output_dir')
     args = parser.parse_args()
-    main(args.root_dir)
+    main(args.root_dir, args.output_dir)
