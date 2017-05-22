c377aa8cc7a49495daf09f859cfd4f44ab119623
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2017-05-22 14:07:19 -0400

    Added scripts for analyzing visual metrics.

---
+++ aft_debug/crop_images.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+
+from PIL import Image
+
+import os
+
+def main(directory, output_dir):
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    for filename in os.listdir(directory):
+        print filename
+        if filename.startswith('ms_'):
+            input_filename = os.path.join(directory, filename)
+            output_filename = os.path.join(output_dir, filename)
+            img = Image.open(input_filename)
+            img2 = img.crop((0, 280, 1440, 2560))
+            img2.save(output_filename)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('dir')
+    parser.add_argument('output_dir')
+    args = parser.parse_args()
+    main(args.dir, args.output_dir)
